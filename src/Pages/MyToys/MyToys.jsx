a321c3d951a3e24cd98e70ace8a37e6a57ea28b8
Your Name <shahanricky86@gmail.com>
2023-05-21 00:52:03 +0600

    handle-delete

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,17 +1,50 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 import Toy from './Toy';
+import Swal from 'sweetalert2';
 
 
 const MyToys = () => {
     const { user } = useContext(AuthContext);
-    const [myToys, setMyToys] = useState([])
-    const url = (`http://localhost:3500/myToys?email=${user.email}`)
+    const [myToys, setMyToys] = useState([]);
+    const [control, setControl] = useState(true)
+
     useEffect(() => {
-        fetch(url)
+        fetch(`http://localhost:3500/myToys?email=${user.email}`)
             .then(res => res.json())
             .then(data => setMyToys(data))
-    }, [url])
+    }, [control]);
+
+    const handleDelete = _id => {
+        console.log(_id)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3500/myToys/${_id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            setControl(!control)
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Toy deleted successfully.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
 
     return (
         <div>
@@ -29,7 +62,7 @@ const MyToys = () => {
                             <th>Toy Name</th>
                             <th>Sub Category</th>
                             <th>Price</th>
-                            <th>Available Quantity</th>
+                            <th>Quantity</th>
                             <th>View</th>
                             <th>Update</th>
                         </tr>
@@ -39,6 +72,7 @@ const MyToys = () => {
                             myToys.map(toy => <Toy
                                 key={toy._id}
                                 toy={toy}
+                                handleDelete= {handleDelete}
                             ></Toy>)
                         }
                     </tbody>
--- src/Pages/MyToys/Toy.jsx
+++ src/Pages/MyToys/Toy.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
-const Toy = ({ toy }) => {
-
+const Toy = ({ toy, handleDelete }) => {
     const { _id, seller, price, category, rating, name, quantity, picture } = toy;
+
+
     return (
         <tr className='text-center'>
             <td>
-                <button className="btn-circle btn bg-[#e6c6d8] border-0 text-black hover:text-white">
+                <button onClick={() => handleDelete(_id)} className="btn-circle btn bg-[#e6c6d8] border-0 text-black hover:text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             </td>
